07ec26f0161c705013b16dec8846718a80de1340
cykreet <cykreet@gmail.com>
2021-10-08 10:05:16 +0100

    fix(wrapper): use undici instead of got

--- packages/wrapper/src/helpers/fetchEndpoint.ts
+++ packages/wrapper/src/helpers/fetchEndpoint.ts
@@ -1,11 +1,17 @@
-import got, { OptionsOfJSONResponseBody } from "got";
+import { request } from "undici";
 import { Endpoint } from "../constants";
 
-export async function fetchEndpoint(host: string, endpoint: Endpoint, options?: OptionsOfJSONResponseBody) {
-  const fullEndpoint = `${host}/${endpoint}`;
-  const request = got(fullEndpoint, options);
-  const data = request.json();
-  const response = await request;
+export type SearchParam = { name: string; value: any };
+type Options = Parameters<typeof request>[1] & { searchParams?: SearchParam[] };
+
+export async function fetchEndpoint(host: string, endpoint: Endpoint, options?: Options) {
+  const fullEndpoint = new URL(endpoint, host);
+  const searchParams = options?.searchParams;
+  if (searchParams) {
+    searchParams.forEach((p) => fullEndpoint.searchParams.append(p.name, p.value));
+  }
+
+  const response = await request(fullEndpoint, options);
   if (response.statusCode !== 200) throw new Error(`Failed to retreive "${endpoint}" data.`);
-  return data;
+  return response.body.json();
 }
